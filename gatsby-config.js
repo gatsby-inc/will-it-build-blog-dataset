ba9cab0df1d875370a9276dc709b7650383d2761
Tyler Barnes <tylerdbarnes@gmail.com>
2020-02-20 11:46:28 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby-Drupal WillItBuild exporter`,
+    description: `Pulls data from Drupal and exports it to JSON`,
     author: `@gatsbyjs`,
   },
   plugins: [
