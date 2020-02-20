146ee26c4f11d1d6c2aa070e8d5cb79da5536708
Tyler Barnes <tylerdbarnes@gmail.com>
2020-02-20 11:39:23 -0800

    set up site to export from gatsby-source-drupal

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,85 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const kebabCase = require(`lodash.kebabcase`)
+const getSlug = require(`speakingurl`)
+const fs = require("fs-extra")
 
-// You can delete this file if you're not using it
+const pluginOptionsUrl = require(`./gatsby-config`).plugins.find(
+  plugin => plugin.resolve === `gatsby-source-drupal`
+).options.baseUrl
+
+const directoryPath = `./json/${getSlug(pluginOptionsUrl)}/`
+
+const saveNodeToDisk = node => {
+  const filePath = `${directoryPath}/articles/${node.id}.json`
+
+  fs.ensureFileSync(filePath)
+  fs.writeJSONSync(filePath, node)
+}
+
+exports.createPages = async ({ graphql, reporter }) => {
+  const result = await graphql(`
+    {
+      articles: allNodeArticle {
+        nodes {
+          drupal_id
+          title
+          created
+          body {
+            processed
+          }
+          field_image {
+            alt
+            title
+            width
+            height
+          }
+          relationships {
+            field_image {
+              filename
+              filemime
+              localFile {
+                childImageSharp {
+                  original {
+                    src
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(result.errors)
+  }
+
+  const normalizedArticles = result.data.articles.nodes.map(node => ({
+    id: node.drupal_id,
+    path: kebabCase(node.title),
+    title: node.title,
+    date: node.created,
+    body: node.body.processed,
+    image: {
+      alt: node.field_image.alt,
+      title: node.field_image.title,
+      width: node.field_image.width,
+      height: node.field_image.height,
+      fileName: node.relationships.field_image.filename,
+      fileMime: node.relationships.field_image.filemime,
+      staticPath:
+        node.relationships.field_image.localFile.childImageSharp.original.src,
+    },
+  }))
+
+  fs.emptyDirSync(directoryPath)
+
+  normalizedArticles.forEach(saveNodeToDisk)
+}
+
+exports.onPostBootstrap = () =>
+  fs.moveSync(`./public/static`, `${directoryPath}/static`, err => {
+    if (err) return console.error(err)
+
+    console.log(`finished exporting ${pluginOptionsUrl} to ./json/!`)
+  })
