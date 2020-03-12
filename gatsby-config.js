29432b0f4abb88a796f9eb6e6958ea4b41fa63bd
Tyler Barnes <tylerdbarnes@gmail.com>
2020-03-12 12:37:22 -0700

    add data set configs to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,15 +8,53 @@ module.exports = {
     {
       resolve: `gatsby-plugin-data-generator`,
       options: {
-        generateSingleSet: `willitbuild-01`,
+        // use this option to generate a single dataset
+        // otherwise it will do all of them one by one
+        // generateSingleSet: `willitbuild-03`,
         sets: [
           {
             name: "willitbuild-01",
-            articles: 64,
+            articles: 512,
           },
           {
             name: "willitbuild-02",
-            articles: 128,
+            articles: 1024,
+          },
+          {
+            name: "willitbuild-02",
+            articles: 2048,
+          },
+          {
+            name: "willitbuild-03",
+            articles: 4096,
+          },
+          {
+            name: "willitbuild-04",
+            articles: 8192,
+          },
+          {
+            name: "willitbuild-05",
+            articles: 16384,
+          },
+          {
+            name: "willitbuild-06",
+            articles: 32768,
+          },
+          {
+            name: "willitbuild-07",
+            articles: 65536,
+          },
+          {
+            name: "willitbuild-08",
+            articles: 131072,
+          },
+          {
+            name: "willitbuild-09",
+            articles: 262144,
+          },
+          {
+            name: "willitbuild-10",
+            articles: 524288,
           },
         ],
       },
